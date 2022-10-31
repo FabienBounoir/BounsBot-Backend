54135bfa09150f7dec8d9b83369c5405b763f477
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-10-31 10:13:24 +0100

    👋 remove element not important for level

--- src/controllers/levelDiscord/getGlobalLevel.js
+++ src/controllers/levelDiscord/getGlobalLevel.js
@@ -1,7 +1,7 @@
 const globalLevel = require("../../models/DiscordGlobalLevel")
 
 module.exports = async function (req, res, next) {
-    const rank = await globalLevel.find({ dataCollection: true }).sort({ xp: -1 }).limit(100).skip(100 * req.query.page).exec()
+    const rank = await globalLevel.find({ dataCollection: true }, { identifiant: -1, username: -1, xp: -1, nbMessage: -1, picture: -1 }).sort({ xp: -1 }).limit(100).skip(100 * req.query.page).exec()
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json({
         rank
